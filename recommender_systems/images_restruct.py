550019d15a541dbca53962c2e79cfdb42ff13677
Hamza Hassan Khan <hamza.334467@yahoo.com>
2023-02-06 13:05:32 +0000

    Use pretrained ResNet50 on Fashion-MNIST dataset and A pretrained VGG19 on a Clothing dataset link provided in the jupyter, evaluated training and validation error,Create another copy for running models in GPU kept the oroginal Juypter Notebook (Fashion_image_classfication) to not lose the output as Runtime is around 18 mins

--- recommender_systems/images_restruct.py
+++ recommender_systems/images_restruct.py
@@ -1,19 +1,17 @@
-import pandas as pd
-import numpy as np
-import shutil
+# import pandas as pd
+# import numpy as np
+# import shutil
 
-import os
+# import os
 
-articles_df = pd.read_csv("datasets/articles.csv")
-
-
-
-
-import os
+# articles_df = pd.read_csv("datasets/articles.csv")
 
 
 
-print(articles_df.shape)
+
+
+
+# print(articles_df.shape)
 # print(articles_df.iloc[0])
 
 # 108775015
@@ -26,19 +24,19 @@ print(articles_df.shape)
 
 """  DOG Wear APpears twice in the loop, when the program create
 the directory for it the second time it crashes saying that it already exist"""
-
-i = 0
-remaining_classes = []
-# for target_class in articles_df['product_type_name'].drop_duplicates().values:
-for target_class in  ['Eyeglasses', 'Wireless earphone case', 'Stain remover spray', 'Clothing mist']:
-    os.mkdir(f'datasets/images/{target_class.replace("/","")}')
-    for j in articles_df['article_id'][articles_df['product_type_name'] == f'{target_class}']:
-        j = str(j)
-        try:
-            shutil.copy(f'datasets/images/0{j[:2]}/0{j}.jpg', f'datasets/images/{target_class}/')
-        except:
-            continue
-    i+=1
-    print(i)
-
-
+#
+# i = 0
+# remaining_classes = []
+# # for target_class in articles_df['product_type_name'].drop_duplicates().values:
+# for target_class in  ['Eyeglasses', 'Wireless earphone case', 'Stain remover spray', 'Clothing mist']:
+#     os.mkdir(f'datasets/images/{target_class.replace("/","")}')
+#     for j in articles_df['article_id'][articles_df['product_type_name'] == f'{target_class}']:
+#         j = str(j)
+#         try:
+#             shutil.copy(f'datasets/images/0{j[:2]}/0{j}.jpg', f'datasets/images/{target_class}/')
+#         except:
+#             continue
+#     i+=1
+#     print(i)
+#
+#
